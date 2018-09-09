5130e36a2bcefef60e4fcdbb56fcf8a43f6779f2
gloriaconcepto <gloriaconcepto@outlook.com>
2018-09-09 23:18:16 +0100

    python cProfile script to perform experiment between Sequential And BinarySearch

---
+++ problem4/cProfileCase1.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name: cProfile results for case when is data[0]
+# Purpose:Educational
+#
+# Author:      mmk  and emeka
+#
+# Created:     06/09/2018
+# Copyright:   (c) mmk 2018
+# Licence:     <gloriaconcepto>
+#-------------------------------------------------------------------------------
+import cProfile
+import SearchingAndSortingA
+
+
+
+
+def main():
+   # pass
+    cProfile.run('SearchingAndSortingA.main()')
+
+if __name__ == '__main__':
+    main()
